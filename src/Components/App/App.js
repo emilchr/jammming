3219ba643e125b0f12530e23f98249ada6db585c
emilchr <98517843+emilchr@users.noreply.github.com>
2023-04-13 16:00:40 +0200

    Step 42

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -42,9 +42,19 @@ export default class App extends React.Component {
           album: 'PlaylistAlbum3',
           id: 33
         }]
-       
-    };
+      };
+      // bindings
+      this.addTrack = this.addTrack.bind(this);
   }
+  
+  addTrack(track) {
+    if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    } else {
+      this.state.playlistTracks.push(track);
+    }
+  }
+
   render() {
     return (
     <div>
@@ -52,7 +62,7 @@ export default class App extends React.Component {
       <div className="App">
          <SearchBar />
         <div className="App-playlist">
-          <SearchResults searchResults={this.state.searchResults} />
+          <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
           <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
         </div>
       </div>
